d5b0ce9bf3d7659d2b4e5edea171087a1cabd281
André Pinheiro <andrepdpinheiro_git@hotmail.com>
2011-09-01 17:32:07 +0100

    Done with sign it

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,6 +6,15 @@ describe "Users" do
     
     describe "failure" do
       
+      it "should not sign a user in" do
+	visit signin_path
+	fill_in :email, :with => ""
+	fill_in :password, :with => ""
+	click_button
+	response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
+      
       it "should not make a new user" do
 	lambda do 
 	  visit signup_path
@@ -21,6 +30,17 @@ describe "Users" do
     end
     
     describe "success" do
+      
+      it "should sign a user in and out" do
+	user = Factory(:user)
+	visit signin_path
+	fill_in :email, :with => user.email
+	fill_in :password, :with => user.password
+	click_button
+	controller.should be_signed_in
+	click_link "Sign out"
+	controller.should_not be_signed_in
+      end
       
       it "should make a new user" do
 	lambda do
