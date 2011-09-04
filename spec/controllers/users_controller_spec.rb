433d1ab1cff589a891fa1915336c9fb62f949b15
André Pinheiro <andrepdpinheiro_git@hotmail.com>
2011-09-04 19:19:55 +0100

    Added user microposts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -34,6 +34,14 @@ describe UsersController do
       get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravatar")
     end
+    
+    it "should show the user's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content 0> "Baz, quux")
+      get :show, :id => @user
+      response.should have_selector("span.content", :content => mp1.content)
+      response.sahould have_selector("span.content", :content => mp2.content)
+    end
   end
   
   describe "GET 'new'" do
